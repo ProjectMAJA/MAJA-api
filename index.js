1e37ccbb94f0f3a3a6b60c70d9329415514ff9f9
AdrienLcp <adrienlcp@gmail.com>
2021-08-24 13:35:08 +0200

    test

--- index.js
+++ index.js
@@ -11,13 +11,14 @@ const app = express();
 app.use(cors());   
 app.use(express.json());
 
-app.use('/v1', router);
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "mon-domaine.fr"),
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"),
     next()
 });
+
+app.use('/v1', router);
 
 app.listen(port, () => {
     console.log(`Server started on http://localhost:${port}`);
